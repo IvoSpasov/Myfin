0022fa35fd474c57d200dd526cb4891cee095e63
Ivo Spasov <ivo.spasov@assentcompliance.com>
2026-06-08 13:48:29 +0300

    add Maven project skeleton with health controller

---
+++ src/main/java/com/p3/myfin/MyfinApplication.java
@@ -0,0 +1,13 @@
+package com.p3.myfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/p3/myfin/web/HealthController.java
@@ -0,0 +1,18 @@
+package com.p3.myfin.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+    @GetMapping
+    public Map<String, Object> health(){
+        return Map.of("status", "UP",
+                "time", System.currentTimeMillis(),
+                "app", "My fin app");
+    }
+}
